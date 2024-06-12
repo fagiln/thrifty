fe891aaef19350ffcfb439740a2d74b7affee638
fagiln <fagilnuril18@gmail.com>
2024-06-12 22:56:37 +0700

    [add] favicon

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Thrifty | @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('assets/img/logo.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('/adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet"
