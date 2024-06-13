5e674a28571de1d3b8a3162cf801ffa28a324363
fagiln <fagilnuril18@gmail.com>
2024-06-13 10:34:05 +0700

    [fix] $item

--- resources/views/buyer/detail.blade.php
+++ resources/views/buyer/detail.blade.php
@@ -28,7 +28,7 @@
                         <div class="p-2 bd-highlight fw-bold">Rp. {{ $product->price }}</div>
                         <div class="p-2 bd-highlight">
                             @auth
-                                <a href="{{route('product.buy', $item->id)}}" class="btn btn-custom"><i class="fas fa-cart-plus"></i> Buy</a>
+                                <a href="{{route('product.buy', $product->id)}}" class="btn btn-custom"><i class="fas fa-cart-plus"></i> Buy</a>
                             @endauth
                             @guest
                                 <a href="/login" class="btn btn-custom"><i class="fas fa-cart-plus"></i> Buy</a>
